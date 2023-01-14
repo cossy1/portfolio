c8bd9309ed2c069472dfa32ac18821344573dfe2
cossy1 <onyekwuocosmas@gmail.com>
2023-01-14 22:41:01 +0100

    wip

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import {ColorConstants} from "@/styles/colorConstants";
-import { BsCodeSlash } from 'react-icons/bs';
-import { CgWebsite } from 'react-icons/cg';
-import {Space} from "antd";
-
+import { ColorConstants } from "@/styles/colorConstants";
+import { BsCodeSlash } from "react-icons/bs";
+import { CgWebsite } from "react-icons/cg";
+import { Space } from "antd";
 
 const StyledExperience = styled.div`
   background: ${ColorConstants.secondary};
@@ -16,69 +15,65 @@ const StyledExperience = styled.div`
   gap: 50px;
   flex-wrap: wrap;
 
-  .experience{
+  .experience {
     background: ${ColorConstants.primary};
     padding: 40px;
     max-width: 250px;
     display: flex;
     justify-content: center;
-    
-    div{
+
+    div {
       display: flex;
       justify-content: center;
       align-items: start;
       flex-direction: column;
       color: ${ColorConstants.tertiary};
       font-size: 20px;
-      
-      h2{
+
+      h2 {
         font-size: 30px;
       }
     }
   }
-  
-  .code{
+
+  .code {
     color: ${ColorConstants.onSecondary};
     max-width: 400px;
   }
-  
-  @media(max-width: 768px){
+
+  @media (max-width: 768px) {
     justify-content: start;
-    //flex-direction: column;
   }
 `;
 
 export const Experience = () => {
-
-    return(
-        <StyledExperience>
-            <div className='experience'>
-                <div>
-                    <h2>3+</h2>
-                    <p>Years</p>
-                    <p>Working</p>
-                    <p>Experience</p>
-                </div>
-            </div>
-            <div className='code'>
-                <Space direction='vertical' size='middle'>
-                    <BsCodeSlash color='white' size={25} />
-                    <h2>Front end Developer</h2>
-                    <p>I specialize in Javascript, Typescript, React.Js, Next.Js</p>
-
-                </Space>
-            </div>
-            <div className='code'>
-                <Space direction='vertical' size='middle'>
-                    <CgWebsite color='white' size={25} />
-                    <h2>Web Developer</h2>
-                    <p>
-                        I design fully functional and responsive website with my professional knowledge on Javascript.
-                    </p>
-
-                </Space>
-            </div>
-
-        </StyledExperience>
-    );
-}
+  return (
+    <StyledExperience>
+      <div className="experience">
+        <div>
+          <h2>3+</h2>
+          <p>Years</p>
+          <p>Working</p>
+          <p>Experience</p>
+        </div>
+      </div>
+      <div className="code">
+        <Space direction="vertical" size="middle">
+          <BsCodeSlash color="white" size={25} />
+          <h2>Front end Developer</h2>
+          <p>I specialize in Javascript, Typescript, React.Js, Next.Js</p>
+        </Space>
+      </div>
+      <div className="code">
+        <Space direction="vertical" size="middle">
+          <CgWebsite color="white" size={25} />
+          <h2>Web Developer</h2>
+          <p>
+            I design fully functional and responsive website with my
+            professional knowledge on Javascript.
+          </p>
+        </Space>
+      </div>
+    </StyledExperience>
+  );
+};
